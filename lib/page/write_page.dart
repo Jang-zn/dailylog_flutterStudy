44d99238b1602da2ec6e7e4af472b6b660c95105
Jang <zn2309@naver.com>
2021-11-17 18:19:32 +0900

    Image.asset으로 이미지 로딩하는거까지함.

--- lib/page/write_page.dart
+++ lib/page/write_page.dart
@@ -13,6 +13,19 @@ class DiaryWritePage extends StatefulWidget {
 
 class _DiaryWritePageState extends State<DiaryWritePage> {
 
+  int imgIndex = 0;
+  List<String> images=[
+    "assets/img/wallpaper1.png",
+    "assets/img/wallpaper2.png",
+    "assets/img/wallpaper3.png"
+  ];
+  List<String> statusImages=[
+    "assets/img/weather1.png",
+    "assets/img/weather2.png",
+    "assets/img/weather3.png",
+    "assets/img/weather4.png"
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -28,9 +41,55 @@ class _DiaryWritePageState extends State<DiaryWritePage> {
       ),
       body:ListView.builder(
        itemBuilder: (ctx, idx){
+         if(idx==0){
+            return InkWell(child:Container(
+              child : Image.asset(widget.diary.image, fit: BoxFit.contain),
+              width:100,
+              height:100,
+              ),
+              onTap: (){
+              widget.diary.image= images[imgIndex];
+              imgIndex++;
+              imgIndex = imgIndex%images.length;
+              setState(() {
+
+              });
+              },
+            );
+         }else if(idx==1){
+           return Container(
+             child:Row(
+               children:List.generate(statusImages.length, (index) {
+                 return InkWell(child:
+                  Container(
+                    child:Image.asset(statusImages[index]),
+                    decoration: BoxDecoration(
+                      color: index==widget.diary.status?Colors.lime:Colors.white,
+                    ),
+                    height:50,
+                    width:50,
+                  ),
+                   onTap: (){
+                   setState(() {
+                     widget.diary.status = index;
+                   });
+                   },
+                 );
+               }),
+             )
+           );
+         }else if(idx==2){
+
+         }else if(idx==3){
+
+         }else if(idx==4){
+
+         }else{
+
+         }
          return Container();
        },
-       itemCount:4,
+       itemCount:6,
 
       )
     );
